bd9d51d0cf1d5424ec19901228f428d11bf26a6c
himanshusoni3214 <45545836+himanshusoni3214@users.noreply.github.com>
2026-06-30 14:50:10 -0400

    Load Chapter 1 layout fixes

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import App from './App';
 import './styles.css';
 import './chapter1.css';
 import './chapter1-fix.css';
+import './chapter1-layout.css';
 import './chapter2.css';
 
 createRoot(document.body.querySelector('#root')!).render(<App />);
