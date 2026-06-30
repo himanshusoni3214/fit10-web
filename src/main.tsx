5cbb3f6247a787d420e3bd223e67e5f89fcac12a
himanshusoni3214 <45545836+himanshusoni3214@users.noreply.github.com>
2026-06-30 14:19:12 -0400

    Load final Chapter 1 fixes

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './chapter1.css';
+import './chapter1-fix.css';
 
 createRoot(document.body.querySelector('#root')!).render(<App />);
