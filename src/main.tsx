df7b87574e100c1e748938ea8412e9128ce23b10
himanshusoni3214 <45545836+himanshusoni3214@users.noreply.github.com>
2026-06-30 14:25:22 -0400

    Load Chapter 2 meal scan styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,6 @@ import App from './App';
 import './styles.css';
 import './chapter1.css';
 import './chapter1-fix.css';
+import './chapter2.css';
 
 createRoot(document.body.querySelector('#root')!).render(<App />);
