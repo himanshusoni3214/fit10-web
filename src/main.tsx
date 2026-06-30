404fa53875498064b45fc1da18fc439cf37f9ad9
himanshusoni3214 <45545836+himanshusoni3214@users.noreply.github.com>
2026-06-30 13:45:29 -0400

    Load Chapter 1 polish styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
+import './chapter1.css';
 
 createRoot(document.body.querySelector('#root')!).render(<App />);
